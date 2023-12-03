f18a403c63beff4e7cf583c06d03052d7d60f57c
ltuananh07 <lanh07102001@gmail.com>
2023-12-03 16:47:31 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Chào em cô gái đổ rịu-.-!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Chạm vào đây này',
+    title: 'Không biết đằng ấy có thể cho anh 1 cái hẹn được không nhở 🥹 ',
+    btnYes: 'Đồng ý đi mà👉👈',
+    btnNo: 'Không đồng ý 😭',
+    question:'Nếu em đồng ý rùi 👉👈 thì hãy cho anh lịch mà em rảnh điiiii',
+    btnReply: 'Xong rùi thì em nhấn vô đây nè 👉👈 ',
+    reply: 'Cuối tuần này em rảnh không hihi',
+    mess: 'Thế thì cuối tuần này chúng ta sẽ gặp nhau nha. Nếu em ngại thì có thể rủ chị yêu quý của anh đi cùng 😁',
+    messDesc: 'Cảm em ơn đã hoàn thành những list vừa rùi 🥹.',
+    btnAccept: 'facebook bạn ý đây nè👉👈',
+    messLink: 'https://www.facebook.com/profile.php?id=100060634248903' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
